4a4a25925c0ec2e89261602496c13e9dff111878
samuelc02 <78934399+samuelc02@users.noreply.github.com>
2022-03-23 13:20:56 +0000

    Update nibbleCounter(SAM).vhd

--- CommandProcessor/nibbleCounter(SAM).vhd
+++ CommandProcessor/nibbleCounter(SAM).vhd
@@ -18,6 +18,7 @@ ARCHITECTURE arc OF dataConversion IS
   SIGNAL convDone: std_logic;   --active HIGH
   TYPE state_type IS (INIT, CONVERSION, SET_FIRST_DIGIT, SEND_FIRST_DIGIT, SEND_SECOND_DIGIT,SET_SECOND_DIGIT,SET_SPACE,SEND_SPACE,WAIT_SEND,ECHO_COMMAND);
   SIGNAL curState, nextState : state_type;
+  SIGNAL firstDigitASCII, secondDigitASCII : std_logic_vector(7 downto 0);
   
 BEGIN
 nextState:PROCESS(curState,DataReady,convDone, TxDone)
@@ -33,11 +34,9 @@ nextState:PROCESS(curState,DataReady,convDone, TxDone)
         END IF;
         
       WHEN ECHO_COMMAND =>
-        nextState <= SEND_COMMAND;
+        nextState <= SEND;
         
-      WHEN SEND_COMMAND =>
-         
-        nextState <= INIT; 
+      
           
       WHEN CONVERSION =>
         IF convDone = '1' AND txDone = '1' THEN
@@ -49,7 +48,7 @@ nextState:PROCESS(curState,DataReady,convDone, TxDone)
       WHEN SEND_FIRST_DIGIT =>
         nextState <= SET_SECOND_DIGIT; 
         
-      WHEN SET_SECOND_FIGIT =>
+      WHEN SET_SECOND_DIGIT =>
         IF txDone = '1' THEN
           nextState <= SEND_SECOND_DIGIT;
         ELSE
@@ -62,20 +61,14 @@ nextState:PROCESS(curState,DataReady,convDone, TxDone)
         
       WHEN SET_SPACE =>
         IF txDone = '1' THEN 
-          nextState <= SEND_SPACE;
+          nextState <= SEND;
         ELSE 
           nextState <= SET_SPACE;
         END IF;
           
-      WHEN SEND_SPACE =>
-        nextState <= WAIT_SEND;
+      WHEN SEND =>  --Anytime we need to send something and the next state is INIT we just use SEND, to reduce the number of total states
+        nextState <= INIT;
       
-      WHEN WAIT_SEND =>
-        IF txDone ='1' THEN
-          nextState <= INIT;
-        ELSE 
-          nextState <= WAIT_SEND;
-        END IF;
         
     END CASE;
   END PROCESS;
@@ -89,4 +82,55 @@ combi_out:PROCESS(curState)
         convDone <= '0';
       WHEN 
         
-
+        
+data_conversion:PROCESS(curState,dataIn)
+  SIGNAL firstDigit,secondDigit : std_logic_vector(3 downto 0); --firstDigit is the most significant digit in the hexadecimal number pair (bit 4 to 7), the first digit to be sent
+  BEGIN
+    IF curState=CONVERSION THEN
+      firstDigit <= dataIn(7 downto 4);
+      secondDigit <= dataIn(3 downto 0);
+      CASE firstDigit IS
+        WHEN "0000" => firstDigitASCII <= "00110000";
+        WHEN "0001" => firstDigitASCII <= "00110001";
+        WHEN "0010" => firstDigitASCII <= "00110010";
+        WHEN "0011" => firstDigitASCII <= "00110011";
+        WHEN "0100" => firstDigitASCII <= "00110100";
+        WHEN "0101" => firstDigitASCII <= "00110101";
+        WHEN "0110" => firstDigitASCII <= "00110110";
+        WHEN "0111" => firstDigitASCII <= "00110111";
+        WHEN "1000" => firstDigitASCII <= "00111000";
+        WHEN "1001" => firstDigitASCII <= "00111001";
+        WHEN "1010" => firstDigitASCII <= "01000001";
+        WHEN "1011" => firstDigitASCII <= "01000010";
+        WHEN "1100" => firstDigitASCII <= "01000011";
+        WHEN "1101" => firstDigitASCII <= "01000100";
+        WHEN "1110" => firstDigitASCII <= "01000101";
+        WHEN "1111" => firstDigitASCII <= "01000110";
+      END CASE;
+      
+      CASE secondDigit IS
+        WHEN "0000" => secondDigitASCII <= "00110000";
+        WHEN "0001" => secondDigitASCII <= "00110001";
+        WHEN "0010" => secondDigitASCII <= "00110010";
+        WHEN "0011" => secondDigitASCII <= "00110011";
+        WHEN "0100" => secondDigitASCII <= "00110100";
+        WHEN "0101" => secondDigitASCII <= "00110101";
+        WHEN "0110" => secondDigitASCII <= "00110110";
+        WHEN "0111" => secondDigitASCII <= "00110111";
+        WHEN "1000" => secondDigitASCII <= "00111000";
+        WHEN "1001" => secondDigitASCII <= "00111001";
+        WHEN "1010" => secondDigitASCII <= "01000001";
+        WHEN "1011" => secondDigitASCII <= "01000010";
+        WHEN "1100" => secondDigitASCII <= "01000011";
+        WHEN "1101" => secondDigitASCII <= "01000100";
+        WHEN "1110" => secondDigitASCII <= "01000101";
+        WHEN "1111" => secondDigitASCII <= "01000110";
+      END CASE;
+      
+  ELSE 
+    firstDigit <= TO_UNSIGNED(3 downto 0);
+    secondDigit <= TO_UNSIGNED (3 downto 0);
+    firstDigitASCII <= TO_UNSIGNED(7 downto 0);
+    secondDigitASCII <= TO_UNSIGNED(7 downto 0);
+    
+  END IF;
